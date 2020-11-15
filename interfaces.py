9adfb64616ba184e6295041499db72cbe4214fba
Adrian Panas <arszonto@gmail.com>
2020-11-15 18:20:37 +0100

    Create 'interfaces' - in python there are no interfaces because they are not needed - duck-typing can be used here

---
+++ interfaces.py
@@ -0,0 +1,15 @@
+class IArithmeticsAdd:
+    def Addition(A,B):
+        raise NotImplementedError()
+
+class IArithmeticsDiff:
+    def Difference(A,B):
+        raise NotImplementedError()
+
+class IArithmeticsMult:
+    def Multiplication(A,B):
+        raise NotImplementedError()
+
+class IArithmeticsDiv:
+    def Division(A,B):
+        raise NotImplementedError()
