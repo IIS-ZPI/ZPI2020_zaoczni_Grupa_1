24b891f83f59acc8b653b769932adc87d94727aa
Bartek Sobol <sobolbartosz@gmail.com>
2020-11-15 19:54:58 +0100

    Revert "Merge pull request #16 from IIS-ZPI/muliplying/first_comment"
    This reverts commit bdc16ce47247b7e55200e283c9cfca2e3e09915f, reversing
    changes made to c51b384dd4bc26e28d9bbe633428d0ad53228c79.

--- Multiply.py
+++ Multiply.py
@@ -1,13 +1,5 @@
-#importing
 from interfaces import IArithmeticsMult 
 
-#multiplication
 class Multiply(IArithmeticsMult):
     def Multiplication(A, B):
-        #returning value
-        if(A>B):
-            print("A is bigger than B")
-            return A*B
-        else:
-            return A*B
-        
+        return A*B
