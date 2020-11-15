ffa35face935dd647366c564d535a0468212de50
Jakub Kolaczynski <kubak95@gmail.com>
2020-11-15 19:30:20 +0100

    Comment 3 - 215174

--- Multiply.py
+++ Multiply.py
@@ -1,5 +1,5 @@
-from interfaces import IArithmeticsMult 
-
+from interfaces import IArithmeticsMult
+#Comment 3 - 215174
 class Multiply(IArithmeticsMult):
     def Multiplication(A, B):
         return A*B
