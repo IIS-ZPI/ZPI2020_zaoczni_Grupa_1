ba13ab1a54554eb6130461ce7dccde2f225cf989
arszonto <arszonto@gmail.com>
2021-01-09 21:22:24 +0100

    #57 Podstawowy interfejs uzytkownika (#62)

--- app/UserInterface.py
+++ app/UserInterface.py
@@ -6,10 +6,31 @@ from app.Util import parse_timeframe
 class UserInterface:
     def show_commands_list(self):
         print("Dostępne opcje:")
-        #TODO: print list of all available commands
+        for idx, command in enumerate(CommandsList):
+            print(f"{idx+1}. {command.description}")
 
     def handle_command(self, command):
-        #TODO: handle command choosen by user
+        if command not in range(1, len(CommandsList)+1):
+            print("Niepoprawna komenda")
+            return
+
+        args = []
+        for argType in CommandsList[command-1].argumentTypes:
+            if argType == ArgumentType.BASE_CURRENCY:
+                args.append(input("Podaj walutę (kod, np. USD): "))
+            elif argType == ArgumentType.COMPARABLE_CURRENCY:
+                args.append(input("Podaj drugą walutę (kod, np. USD): "))
+            elif argType in [ArgumentType.TIMEFRAME_ALL, ArgumentType.TIMEFRAME_LONG]:
+                print_available_timeframes(argType)
+                timeframe = parse_timeframe(input("Podaj okres czasu:"), argType)
+                if timeframe == TimeframeType.INVALID:
+                    print("Niepoprawny argument")
+                    return
+                args.append(timeframe)
+
+        print("Wynik: ")
+        CommandsList[command-1].method(*args)
+        input("Wciśnij enter aby kontynuować...")
 
     def listen_for_user_input(self):
         while True:
