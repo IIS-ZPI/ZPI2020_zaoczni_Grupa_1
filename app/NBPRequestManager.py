c85134018730303c60bc24906e00f0ebbbeb5ef4
arszonto <arszonto@gmail.com>
2021-01-09 21:21:33 +0100

    #38 Szkielet aplikacji
    * #38 Szkielet aplikacji

---
+++ app/NBPRequestManager.py
@@ -0,0 +1,12 @@
+class NBPRequestManager:
+    def get_sessions(currency, timeframe):
+        #TODO
+        pass
+
+    def get_statistics(currency, timeframe):
+        #TODO
+        pass
+
+    def get_ratio_changes(currency_one, currency_two, timeframe):
+        #TODO
+        pass
