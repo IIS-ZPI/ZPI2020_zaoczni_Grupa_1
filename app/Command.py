ecb012d0e48e1c8502144e01571749136f796333
Adrian Panas <arszonto@gmail.com>
2021-01-09 20:49:25 +0100

    #57 Podstawowy interfejs uzytkownika

---
+++ app/Command.py
@@ -0,0 +1,6 @@
+class Command:
+    def __init__(self, name, method, description, argumentTypes):
+        self.name = name
+        self.method = method
+        self.description = description
+        self.argumentTypes = argumentTypes
