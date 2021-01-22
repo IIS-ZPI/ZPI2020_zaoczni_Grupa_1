326b5e0ae31983ee638708be06e05b31f41ad2ee
sable <sable@DESKTOP-F96TDGA>
2021-01-22 10:52:43 +0100

    #41 Obliczanie statystyk

--- app/DataParser.py
+++ app/DataParser.py
@@ -1,3 +1,5 @@
+import statistics
+
 class DataParser:
     def parse_session(session_data):
         if not session_data:
@@ -28,7 +30,31 @@ class DataParser:
         return {"increase": increase, "decrease": decrease, "stable": stable}
     
     def parse_statistics(statistics_data):
-        pass
+        if not statistics_data:
+            return None
+
+        values = []
+
+        try:
+            for element in statistics_data['rates']:
+                values.append(element.get("mid"))  
+        except:
+            print("Błąd parsowania danych")
+            return None
+        
+
+        rounded_values_list = [round(x,2) for x in values] #rounding values to 2 elements after coma
+        rounded_values_list.sort() #sorting in ascending order (for median value)
+
+        mean = sum(rounded_values_list)/len(rounded_values_list)
+
+        median_value = statistics.median(rounded_values_list)
+        mode_value = statistics.mode(rounded_values_list)
+        std_dev_value = round(statistics.stdev(rounded_values_list), 3)
+        coef_var = round((std_dev_value/mean)*100, 3)
+  
+        return {"median": median_value , "mode": mode_value, "standard deviation value": std_dev_value, "coeficient of Variation": coef_var}        
+
 
     def parse_ratio_changes(ratio_changes_data):
         pass
--- app/DataPresenter.py
+++ app/DataPresenter.py
@@ -11,8 +11,11 @@ class DataPresenter:
             print(f"Dni wzrostu: {results['increase']}\nDni spadkowe: {results['decrease']}\nDni bez zmian: {results['stable']}\n")         
         
     def show_statistics(currency, timeframe):
-        statistics_data = NBPRequestManager.get_statistics(currency, timeframe)
-        #TODO: Parse and present
+        statistics_data = NBPRequestManager.get_statistics(currency, timeframe).json()
+        results = DataParser.parse_statistics(statistics_data)
+
+        if results:
+            print(f"Mediana wyników: {results['median']}\nDominanta wyników: {results['mode']}\nOdchylenie standardowe zbioru : {results['standard deviation value']}\nWspółczynnik zmienności zbioru : {results['coeficient of Variation']}\n")
 
     def show_ratio_changes(currency_one, currency_two, timeframe):
         ratio_changes_data = NBPRequestManager.get_ratio_changes(currency_one, currency_two, timeframe)
