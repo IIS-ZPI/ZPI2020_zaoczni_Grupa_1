a4a23beaacae33f7c4f2766ae4507994b837a363
arszonto <arszonto@gmail.com>
2021-02-10 22:52:31 +0100

    #102 Refaktoryzacja istniejących funkcjonalności

--- app/UserInterface.py
+++ app/UserInterface.py
@@ -6,33 +6,34 @@ from app.Util import parse_timeframe
 class UserInterface:
     def show_commands_list(self):
         print("Dostępne opcje:")
-        for idx, command in enumerate(CommandsList):
-            print(f"{idx+1}. {command.description}")
+        for idx, commandNr in enumerate(CommandsList):
+            print(f"{idx+1}. {commandNr.description}")
 
-    def handle_command(self, command):
-        if command not in range(1, len(CommandsList)+1):
+    def handle_command(self, commandNr):
+        if commandNr not in range(1, len(CommandsList)+1):
             print("Niepoprawna komenda")
             return
 
         args = []
-        for argType in CommandsList[command-1].argumentTypes:
+        for argType in CommandsList[commandNr-1].argumentTypes:
             try:
-                if argType == ArgumentType.BASE_CURRENCY:
-                    args.append(input("Podaj walutę (kod, np. USD): "))
-                elif argType == ArgumentType.COMPARABLE_CURRENCY:
-                    args.append(input("Podaj drugą walutę (kod, np. USD): "))
-                elif argType in [ArgumentType.TIMEFRAME_ALL, ArgumentType.TIMEFRAME_LONG]:
-                    print_available_timeframes(argType)
-                    timeframe = parse_timeframe(input("Podaj okres czasu:"), argType)
-                    if timeframe == TimeframeType.INVALID:
-                        print("Niepoprawny argument")
-                        return
-                    args.append(timeframe)
+              if argType == ArgumentType.BASE_CURRENCY:
+                  args.append(input("Podaj walutę (kod, np. USD): "))
+              elif argType == ArgumentType.COMPARABLE_CURRENCY:
+                  args.append(input("Podaj drugą walutę (kod, np. USD): "))
+              elif argType in [ArgumentType.TIMEFRAME_ALL, ArgumentType.TIMEFRAME_LONG]:
+                  print_available_timeframes(argType)
+                  timeframe = parse_timeframe(input("Podaj okres czasu:"), argType)
+                  if timeframe == TimeframeType.INVALID:
+                      print("Niepoprawny argument")
+                      return
+                  args.append(timeframe)
             except EOFError as error:
                 print("Niepoprawny argument")
 
-        print("Wynik: ")
-        CommandsList[command-1].method(*args)
+        result = CommandsList[commandNr-1].method(*args)
+        if result:
+            print(f"Wynik: {result}")
         input("Wciśnij enter aby kontynuować...")
 
     def listen_for_user_input(self):
