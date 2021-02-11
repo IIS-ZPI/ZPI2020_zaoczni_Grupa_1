957c56c342ce6907ac2b4b06ed897104511e394a
ablesable <sobolbartosz@gmail.com>
2021-02-11 03:04:17 +0100

    #89 Naprawa ostrzezen analizy statycznej - fix E999

--- app/DataPresenter.py
+++ app/DataPresenter.py
@@ -8,8 +8,7 @@ class DataPresenter:
         results = DataParser.parse_session(session_data)
 
         if results:
-            return f"Dni wzrostu: {results['increase']}\nDni spadkowe: 
-            {results['decrease']}\nDni bez zmian: {results['stable']}"
+            return f"""Dni wzrostu: {results['increase']}\nDni spadkowe: {results['decrease']}\nDni bez zmian: {results['stable']}"""
 
     def show_statistics(currency, timeframe):
         statistics_data = NBPRequestManager.get_statistics(currency, timeframe).json()
