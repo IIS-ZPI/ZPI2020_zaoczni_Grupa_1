419cdd5c906ff50930bedc46fbeeffdec60fd8fb
Jakub Kolaczynski <kubak95@gmail.com>
2021-02-13 16:56:34 +0100

    #73 - Testy funkcjonalne

---
+++ test/integrationtest/test_ratio_changes.py
@@ -0,0 +1,24 @@
+from re import A
+from unittest import TestCase
+
+from app.NBPRequestManager import NBPRequestManager
+from app.DataParser import DataParser
+from app.TimeframeType import TimeframeType
+
+
+class TestRatioChangesIntegration(TestCase):
+    def test_ratio_changes_request_parse_scenario(self):
+        test_currency_one = "USD"
+        test_currency_two = "EUR"
+        test_timeframe = TimeframeType.WEEK
+
+        ratio_changes_data_one = NBPRequestManager.get_ratio_changes(
+            test_currency_one, test_timeframe).json()
+        ratio_changes_data_two = NBPRequestManager.get_ratio_changes(
+            test_currency_two, test_timeframe).json()
+
+        results = DataParser.parse_ratio_changes(
+            ratio_changes_data_one, ratio_changes_data_two)
+        results_regex = "-?[0-9]+.[0-9]+"
+
+        self.assertRegex(str(results), results_regex)
---
+++ test/integrationtest/test_statistics.py
@@ -0,0 +1,20 @@
+from re import A
+from unittest import TestCase
+
+from app.NBPRequestManager import NBPRequestManager
+from app.DataParser import DataParser
+from app.TimeframeType import TimeframeType
+
+
+class TestStatisticsIntegration(TestCase):
+    def test_statistics_request_parse_scenario(self):
+        test_currency = "USD"
+        test_timeframe = TimeframeType.WEEK
+
+        statistics_data = NBPRequestManager.get_statistics(
+            test_currency, test_timeframe).json()
+
+        results = DataParser.parse_statistics(statistics_data)
+        results_regex = "{'median': [0-9]+.?[0-9]+, 'mode': [0-9]+.?[0-9]+, 'standard deviation value': [0-9]+.?[0-9]+, 'coeficient of Variation': [0-9]+.?[0-9]+}"
+
+        self.assertRegex(str(results), results_regex)
