322f93167ed139bbfc5004c8faf449f0db6ef6f5
arszonto <arszonto@gmail.com>
2021-02-14 16:01:58 +0100

    Usunięcie zbędnych plików

--- Multiply.py
+++
@@ -1,6 +0,0 @@
-from interfaces import IArithmeticsMult 
-
-class Multiply(IArithmeticsMult):
-    def Multiplication(A, B):
-        #returning value
-        return A*B
--- difference.py
+++
@@ -1,7 +0,0 @@
-from interfaces import IArithmeticsDifference
-
-class ArithmeticsDifference(IArithmeticsDiff):
-    def Difference(A,B):
-        return A-B
-
-# Comment Zad 6 - 215174
